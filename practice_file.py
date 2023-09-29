db6fdafaf37da7a4f895ed253f33065168f2d46f
Chalice469 <144635304+Chalice469@users.noreply.github.com>
2023-09-29 18:39:19 -0400

    fixed indent on line 6 of  practice_file.py

--- practice_file.py
+++ practice_file.py
@@ -4,7 +4,7 @@ import pyautogui as pg
 def move(coordinates, time, rest, comment):
     """ this function take composes a line of the program's code
     pair = 100, 100
->>> move(pair, ".5", "1", "line 1 is for declaring the funtion name)
+    >>> move(pair, ".5", "1", "line 1 is for declaring the funtion name)
     moveTo(100,100, duration =.5); sleep(1) #line 1 is for declaring the funtion name:
     """
     x, y = coordinates
