b9adc1fbe028cbfaef434116130b6bb9bd765084
Chalice469 <144635304+Chalice469@users.noreply.github.com>
2023-09-29 19:05:45 -0400

    fixed practice_file.py doc test

--- practice_file.py
+++ practice_file.py
@@ -3,9 +3,12 @@ import pyautogui as pg
 
 def move(coordinates, time, rest, comment):
     """ this function take composes a line of the program's code
-    pair = 100, 100
-    >>> move(pair, ".5", "1", "line 1 is for declaring the funtion name)
-    moveTo(100,100, duration =.5); sleep(1) #line 1 is for declaring the funtion name:
+    #pair = 100, 100
+    >>> move(pair, ".5", "1", "line 1 is for declaring the funtion name")
+    'pg.moveTo(100, 100, duration =.5); sleep(1) #line 1 is for declaring the funtion name;'
     """
     x, y = coordinates
-    return "moveTo({}, {}, duration ={}); sleep({}) #{};".format(x, y, time, rest, comment)
+    return "pg.moveTo({}, {}, duration ={}); sleep({}) #{};".format(x, y, time, rest, comment)
+
+pair = 100, 100
+print(move(pair, '.5', '1', 'line 1 is for declaring the funtion name'))
